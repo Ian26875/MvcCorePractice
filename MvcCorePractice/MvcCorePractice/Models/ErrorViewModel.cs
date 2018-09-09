000a66f30a22254493b9477f594b26fb7d3c9b33
Ian_Chen <fds26875@hotmail.com.tw>
2018-09-09 14:09:33 +0800

    建立Mvc Dotnet Core 專案

---
+++ MvcCorePractice/MvcCorePractice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcCorePractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
